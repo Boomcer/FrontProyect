58ba15120570d660524c543633029054ddc9c8e7
Nicolas_Gallardo <gallardo41299@gmail.com>
2024-10-26 11:56:25 -0300

    terminando de dejar puesta la disposicion de los elementos en la pagina home, esperando al back para probar

--- FrontProyecto/src/views/ProductScreen.jsx
+++ FrontProyecto/src/views/ProductScreen.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import imgDefault from '../assets/imgDefault.jpg'
+import { useState } from 'react';
 
 const ProductScreen = () => {
+
+  const [favorito, setFavorito] = useState(false);
+
+  const toggleFavorito = () =>{
+    setFavorito(!favorito)
+  }
+
   return (
-    <div>
-      <h1>Vista del Producto</h1>
-      <p>Aquí se mostrará la información detallada del producto.</p>
+    <div className='container-fluid vw-100'>
+      <div className='row d-flex align-items-center justify-content-center'>
+        <section className='col-6 d-flex' style={{justifyContent: 'center'}}>
+          <img src={imgDefault} alt="imagen" />
+          <button onClick={toggleFavorito} style={{ border: 'none', background: 'none', cursor: 'pointer', alignSelf: 'flex-start' }}>
+              {favorito ? (
+                <i className="bi bi-heart-fill" style={{ fontSize: '2rem', color: 'aqua' }}></i>
+              ) : (
+                <i className="bi bi-heart" style={{ fontSize: '2rem', color: 'aqua' }}></i>
+              )}
+            </button>
+        </section>
+        <section className='col-5 text-center'>
+          <div>
+            <h1>Nombre producto</h1>
+            <h2>Precio</h2>
+          </div>
+          <div>
+            <div className='row mx-5 gap-3'>
+              <button className='btn btn-primary'>Comprar ahora</button>
+              <button className='btn btn-primary'>Agregar al carrito</button>
+            </div>
+          </div>
+        </section>
+        <section className='row mx-5 px-5'>
+              <h2>Descripcion</h2>
+              <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio odio earum, ducimus eos molestiae autem ea impedit doloremque praesentium explicabo debitis totam aliquam doloribus pariatur non nulla libero nostrum cupiditate?</h4>
+        </section>
+      </div>
+
     </div>
   );
 }
