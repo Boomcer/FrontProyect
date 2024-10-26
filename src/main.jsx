1b2940d01676748cefc51091f637f9b634bebb3c
Daniel <carlosdanielledesma76@gmail.com>
2024-10-26 15:38:26 +0200

    estilos componenetes Card y paginacion

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import './index.css'
-import HomeScreen from './views/HomeScreen.jsx'
+import LoginApp from './components/LoginApp'
+
 
 createRoot(document.getElementById('root')).render(
-   <HomeScreen />
+
+   <div>
+      <LoginApp />
+      </div>
 )
